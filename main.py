71d2cf812b306d073748a2dd0c411e4ddc7355fb
zubxxr <zubxxr@gmail.com>
2022-02-25 15:01:41 -0500

    .........

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-# This is a sample Python script.
-#shahabzafar
-#shahabzafar
-#fdsgd
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#higuys
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from PIL import Image
