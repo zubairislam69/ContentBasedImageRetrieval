9aa71f45ab3db8f9e154a8bd3e667f733d40dfca
106abd <abdullah.106abd@gmail.com>
2022-03-30 19:48:13 -0400

    Whole code in a while loop, asks user if they want to search another image

--- Barcode_Generator.py
+++ Barcode_Generator.py
@@ -7,145 +7,138 @@ import time
 from PIL import Image
 import seaborn as sns
 
-restart = True
+start = time.time()
 
-while restart:
-    start = time.time()
+picNum = 0
+fileNum = 0
 
-    picNum = 0
-    fileNum = 0
+sns.set()
 
-    sns.set()
+counter = 0
+d = {}
+dictionary = []
 
-    counter = 0
-    d = {}
-    dictionary = []
+file = open("barcodes.txt", "w")
 
-    file = open("barcodes.txt", "w")
+for i in range(10):
+    for j in range(10):
+        d[counter] = 'img_' + str(i) + str(j) + '.jpg'
+        counter = counter + 1
 
-    for i in range(10):
-        for j in range(10):
-            d[counter] = 'img_' + str(i) + str(j) + '.jpg'
-            counter = counter + 1
+for i in range(100):
+    image_name = d[picNum]
+    image_path = os.path.join(os.getcwd(), 'MNIST_DS/' + str(fileNum), image_name)
+    # print(image_path)
+    image = Image.open(image_path)
+    arr = np.asarray(image)
 
-    for i in range(100):
-        image_name = d[picNum]
-        image_path = os.path.join(os.getcwd(), 'MNIST_DS/' + str(fileNum), image_name)
-        # print(image_path)
-        image = Image.open(image_path)
-        arr = np.asarray(image)
+# Projection 1
+    proj_1 = []
 
-    # Projection 1
-        proj_1 = []
+    for r in range(28):
+        proj_1.append(sum(arr[r]))
 
-        for r in range(28):
-            proj_1.append(sum(arr[r]))
+    # print("\nProjection 1:")
+    # print(proj_1)
 
-        # print("\nProjection 1:")
-        # print(proj_1)
+# Projection 2
+    proj_2 = []
+    for r in range(26, -27, -1):
+        proj_2.append(sum(np.diagonal(arr, r)))
 
-    # Projection 2
-        proj_2 = []
-        for r in range(26, -27, -1):
-            proj_2.append(sum(np.diagonal(arr, r)))
+    # print("\nProjection 2")
+    # print(proj_2)
 
-        # print("\nProjection 2")
-        # print(proj_2)
+# Projection 3
+    proj_3 = []
+    for r in range(27, -1, -1):
+        proj_3.append(sum(arr[:, r]))
 
-    # Projection 3
-        proj_3 = []
-        for r in range(27, -1, -1):
-            proj_3.append(sum(arr[:, r]))
+    # print("\nProjection 3")
+    # print(proj_3)
 
-        # print("\nProjection 3")
-        # print(proj_3)
+# Projection 4
+    proj_4 = []
+    for r in range(-26, 27):
+        proj_4.append(sum(np.fliplr(arr).diagonal(r)))
 
-    # Projection 4
-        proj_4 = []
-        for r in range(-26, 27):
-            proj_4.append(sum(np.fliplr(arr).diagonal(r)))
+    # print("\nProjection 4")
+    # print(proj_4)
 
-        # print("\nProjection 4")
-        # print(proj_4)
+# Get average of each projection
+    def average(p):
+        proj_average = round((sum(p) / len(p)), 0)
+        return proj_average
 
-    # Get average of each projection
-        def average(p):
-            proj_average = round((sum(p) / len(p)), 0)
-            return proj_average
+    # print("\nAverage of P1")
+    # print(average(proj_1))
+    #
+    # print("Average of P2")
+    # print(average(proj_2))
+    #
+    # print("Average of P3")
+    # print(average(proj_3))
+    #
+    # print("Average of P4")
+    # print(average(proj_4))
 
-        # print("\nAverage of P1")
-        # print(average(proj_1))
-        #
-        # print("Average of P2")
-        # print(average(proj_2))
-        #
-        # print("Average of P3")
-        # print(average(proj_3))
-        #
-        # print("Average of P4")
-        # print(average(proj_4))
-
-    # Convert to 1 and 0
-        def generate_c(p):
-            c = []
-            for item in p:
-                if item > average(p):
-                    c.append(1)
-                else:
-                    c.append(0)
-            return c
+# Convert to 1 and 0
+    def generate_c(p):
+        c = []
+        for item in p:
+            if item > average(p):
+                c.append(1)
+            else:
+                c.append(0)
+        return c
 
 
-        c1 = generate_c(proj_1)
-        c2 = generate_c(proj_2)
+    c1 = generate_c(proj_1)
+    c2 = generate_c(proj_2)
 
-        c3 = generate_c(proj_3)
-        c4 = generate_c(proj_4)
+    c3 = generate_c(proj_3)
+    c4 = generate_c(proj_4)
 
-        # print('c1:', c1)
-        # print('c2:', c1)
-        # print('c3:', c3)
-        # print('c4:', c1)
+    # print('c1:', c1)
+    # print('c2:', c1)
+    # print('c3:', c3)
+    # print('c4:', c1)
 
-        barcode = c1 + c2 + c3 + c4
+    barcode = c1 + c2 + c3 + c4
 
-        print("\nbarcode for " + image_name)
-        print(barcode)
+    print("\nbarcode for " + image_name)
+    print(barcode)
 
 
-    # length of barcode = 162
-        h = ""
-        for i in range(162):
-            h += str(barcode[i])
+# length of barcode = 162
+    h = ""
+    for i in range(162):
+        h += str(barcode[i])
 
-        file.write("barcode for " + str(image_name) + ": ")
-        file.write(h)
-        file.write("\n")
-        file.write("\n")
+    file.write("barcode for " + str(image_name) + ": ")
+    file.write(h)
+    file.write("\n")
+    file.write("\n")
 
 
 
-        print()
+    print()
 
-        # Add all barcode to dictionary with key as image name and value as barcode
-        nameAppend = {image_name: barcode}
-        dictionary.append(nameAppend)
-        # print(dictionary)
+    # Add all barcode to dictionary with key as image name and value as barcode
+    nameAppend = {image_name: barcode}
+    dictionary.append(nameAppend)
+    # print(dictionary)
 
-        picNum = picNum + 1
+    picNum = picNum + 1
 
-        if picNum % 10 == 0:
-            fileNum = fileNum + 1
+    if picNum % 10 == 0:
+        fileNum = fileNum + 1
 
-    print(dictionary)
-    file.close()
+print(dictionary)
+file.close()
 
-    end = time.time()
+end = time.time()
 
-    total = end - start
-    print("\n")
-    print(total)
-
-    answer = input("Would you like to search for another image? ('Y' for yes and 'N' for no)")
-    if answer == "N" or answer == "n":
-        restart = False
+total = end - start
+print("\n")
+print(total)
--- Search_Algorithm.py
+++ Search_Algorithm.py
@@ -54,25 +54,30 @@ def Check_Input_Number(list_barcode, Number):
 def replace_values(list_to_replace, item_to_replace, item_to_replace_with):
     return [item_to_replace_with if item == item_to_replace else item for item in list_to_replace]
 
+restart = True
 
-file = open('barcodes.txt', 'r')  # Read The File (barcodes.txt)
-List_barcode = Get_Barcodes_Data_List(file)  # read the read into file and append the List
-number = input('Enter the Image Number that you want to compare from 00-99 : ')  # Get the image number from user
-print("\n")
-check_res = Check_Input_Number(List_barcode, number)  # Check the Input Image number
-
-if (check_res == 1):
-    Distance_List = Find_Distance(List_barcode, number)  # Find the hamming distance
-    Distance_List = replace_values(Distance_List, 0, 1000)  # Replace the 0 , 1000
-    print('Hamming Distances between the barcodes of the Image Number entered and the rest of the Images : \n', Distance_List)  # Display all the Hamming Distances
+while restart:
+    file = open('barcodes.txt', 'r')  # Read The File (barcodes.txt)
+    List_barcode = Get_Barcodes_Data_List(file)  # read the read into file and append the List
+    number = input('Enter the Image Number that you want to compare from 00-99 : ')  # Get the image number from user
     print("\n")
-    minimum_hamming_distance = min(Distance_List)  # Find the minimum Number
-    minimum_index = Distance_List.index(min(Distance_List))  # Find the minimum number Index
-    Image_Data = List_barcode[minimum_index][2]  # Get The Image Data of minimum number
-    print('The most similar Image is : {} and the Hamming Distance of the Image is: {}'.format(Image_Data,
-                                                                   minimum_hamming_distance))  # Display the Data
+    check_res = Check_Input_Number(List_barcode, number)  # Check the Input Image number
 
-else:
-    print('Please Enter Correct Number : ')
+    if (check_res == 1):
+        Distance_List = Find_Distance(List_barcode, number)  # Find the hamming distance
+        Distance_List = replace_values(Distance_List, 0, 1000)  # Replace the 0 , 1000
+        print('Hamming Distances between the barcodes of the Image Number entered and the rest of the Images : \n', Distance_List)  # Display all the Hamming Distances
+        print("\n")
+        minimum_hamming_distance = min(Distance_List)  # Find the minimum Number
+        minimum_index = Distance_List.index(min(Distance_List))  # Find the minimum number Index
+        Image_Data = List_barcode[minimum_index][2]  # Get The Image Data of minimum number
+        print('The most similar Image is : {} and the Hamming Distance of the Image is: {}'.format(Image_Data,
+                                                                       minimum_hamming_distance))  # Display the Data
 
+    else:
+        print('Please Enter Correct Number : ')
+
+    answer = input("Would you like to search for another image? ('Y' for yes and 'N' for no)")
+    if answer == "N" or answer == "n":
+        restart = False
 
