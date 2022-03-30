0e4030f7df299b64a5b93c7ab6b6bbcdd1eecf9b
shahabzafar <syed.shahabzafar@ontariotechu.net>
2022-03-30 00:23:14 -0400

    Hamming Distance!!

--- Search_Algorithm.py
+++ Search_Algorithm.py
@@ -19,30 +19,60 @@ def Get_Barcodes_Data_List(File):
 
     while line:
         line = line.strip()
-        split_data = line.split(':')
+        split_data = line.split(' ')
         split_list.append(split_data)
         line = File.readline()
         line = File.readline()
-
     return split_list
 
-# Calculating hamming distance between each barcode and putting them in an array
-def Find_Distance(Barcode_List):
+
+def Find_Distance(Barcode_List, number):
     size_list_barcode = len(Barcode_List)
-    str1 = List_barcode[0][1]
+    str1 = Barcode_List[int(number)][3]
     distance_list = []
 
-    for x in range(1, size_list_barcode):
-        str2 = Barcode_List[x][1]
+    for x in range(0, size_list_barcode):
+        str2 = Barcode_List[x][3]
         res = hamming_distance(str1, str2)
-        str1 = str2
         distance_list.append(res)
 
     return distance_list
 
 
-file = open('barcodes.txt', 'r')
-List_barcode = Get_Barcodes_Data_List(file)
-Distance_List = Find_Distance(List_barcode)
-file.close()
-print(Distance_List)
+def Check_Input_Number(list_barcode, Number):
+    check = 0
+    for x in range(len(list_barcode)):
+        compare_data = list_barcode[x][2][4:6]
+        if (number == compare_data):
+            check = 1
+            return check
+        else:
+            check = -1
+    return check
+
+
+def replace_values(list_to_replace, item_to_replace, item_to_replace_with):
+    return [item_to_replace_with if item == item_to_replace else item for item in list_to_replace]
+
+
+file = open('barcodes.txt', 'r')  # Read The File (barcodes.txt)
+List_barcode = Get_Barcodes_Data_List(file)  # read the read into file and append the List
+number = input('Enter the Image Number that you want to compare from 00-99 : ')  # Get the image number from user
+print("\n")
+check_res = Check_Input_Number(List_barcode, number)  # Check the Input Image number
+
+if (check_res == 1):
+    Distance_List = Find_Distance(List_barcode, number)  # Find the hamming distance
+    Distance_List = replace_values(Distance_List, 0, 1000)  # Replace the 0 , 1000
+    print('Hamming Distances between the barcodes of the Image Number entered and the rest of the Images : \n', Distance_List)  # Display all the Hamming Distances
+    print("\n")
+    minimum_hamming_distance = min(Distance_List)  # Find the minimum Number
+    minimum_index = Distance_List.index(min(Distance_List))  # Find the minimum number Index
+    Image_Data = List_barcode[minimum_index][2]  # Get The Image Data of minimum number
+    print('The most similar Image is : {} and the Hamming Distance of the Image is: {}'.format(Image_Data,
+                                                                   minimum_hamming_distance))  # Display the Data
+
+else:
+    print('Please Enter Correct Number : ')
+
+
